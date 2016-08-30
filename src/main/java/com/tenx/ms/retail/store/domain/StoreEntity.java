23391ce6b15e23e68fabffc7bafb542ab74073ab
Juan C. Diaz <jdiaz@ten-x.com>
2016-08-30 10:33:54 -0700

    Defined Store fields

--- src/main/java/com/tenx/ms/retail/store/domain/StoreEntity.java
+++ src/main/java/com/tenx/ms/retail/store/domain/StoreEntity.java
@@ -1,5 +1,25 @@
 package com.tenx.ms.retail.store.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+
+@Entity
+@Table(name = "stores")
 public class StoreEntity {
+    @Id
+    @GeneratedValue
+    private long storeId;
+    private String name;
+
+
+    public long getStoreId() { return this.storeId; }
+
+    public void setStoreId(long storeId) { this.storeId = storeId; }
+
+    public String getName() { return this.name; }
+
+    public void setName(String name) { this.name = name; }
 }
--- src/main/java/com/tenx/ms/retail/store/rest/dto/Store.java
+++ src/main/java/com/tenx/ms/retail/store/rest/dto/Store.java
@@ -1,5 +1,22 @@
 package com.tenx.ms.retail.store.rest.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 
+@ApiModel("Store model")
 public class Store {
+    @ApiModelProperty(value = "The Id of the Store", readOnly = true)
+    private long storeId;
+
+    @ApiModelProperty(value = "The name of the Store")
+    private String name;
+
+
+    public long getStoreId() { return this.storeId; }
+
+    public void setStoreId(long storeId) { this.storeId = storeId; }
+
+    public String getName() { return this.name; }
+
+    public void setName(String name) { this.name = name; }
 }
