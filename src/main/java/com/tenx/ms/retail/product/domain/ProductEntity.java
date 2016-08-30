2953a6041bc413f3a60030e0c83aae1d2fd5f980
Juan C. Diaz <jdiaz@ten-x.com>
2016-08-30 11:24:01 -0700

    Added fields to product entities

--- src/main/java/com/tenx/ms/retail/product/domain/ProductEntity.java
+++ src/main/java/com/tenx/ms/retail/product/domain/ProductEntity.java
@@ -1,11 +1,46 @@
 package com.tenx.ms.retail.product.domain;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.persistence.Table;
-
 
 
 @Entity
 @Table(name = "products")
 public class ProductEntity {
+
+    @Id
+    @GeneratedValue
+    private long productId;
+    private long storeId;
+    private String name;
+    private String description;
+    private String sku;
+    private double price;
+
+
+    public long getProductId() { return this.productId; }
+
+    public void setProductId(long productId) { this.productId = productId; }
+
+    public long getStoreId() { return this.storeId; }
+
+    public void setStoreId(long storeId) { this.storeId = storeId; }
+
+    public String getName() { return this.name; }
+
+    public void setName(String name) { this.name = name; }
+
+    public String getDescription() { return this.description; }
+
+    public void setDescription(String description) { this.description = description; }
+
+    public String getSku() { return this.sku; }
+
+    public void setSku(String sku) { this.sku = sku; }
+
+    public double getPrice() { return this.price; }
+
+    public void setPrice(double price) { this.price = price; }
 }
--- src/main/java/com/tenx/ms/retail/product/rest/dto/Product.java
+++ src/main/java/com/tenx/ms/retail/product/rest/dto/Product.java
@@ -1,5 +1,58 @@
 package com.tenx.ms.retail.product.rest.dto;
 
+import com.tenx.ms.commons.validation.constraints.DollarAmount;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+
+@ApiModel("Product Model")
 public class Product {
+    @ApiModelProperty(value = "The Id of the product.", readOnly = true)
+    private long productId;
+
+    @ApiModelProperty("The Id of the store the product belongs to.")
+    private long storeId;
+
+    @ApiModelProperty("The name of the product.")
+    private String name;
+
+    @ApiModelProperty("The product description.")
+    private String description;
+
+    @ApiModelProperty("The Stock Keeping Unit.")
+    @Pattern(regexp = "^[A-Za-z0-9]*$")
+    @Size(min = 5, max = 10)
+    private String sku;
+
+    @ApiModelProperty("The product price.")
+    @DollarAmount
+    private double price;
+
+
+    public long getProductId() { return this.productId; }
+
+    public void setProductId(long productId) { this.productId = productId; }
+
+    public long getStoreId() { return this.storeId; }
+
+    public void setStoreId(long storeId) { this.storeId = storeId; }
+
+    public String getName() { return this.name; }
+
+    public void setName(String name) { this.name = name; }
+
+    public String getDescription() { return this.description; }
+
+    public void setDescription(String description) { this.description = description; }
+
+    public String getSku() { return this.sku; }
+
+    public void setSku(String sku) { this.sku = sku; }
+
+    public double getPrice() { return this.price; }
+
+    public void setPrice(double price) { this.price = price; }
 }
